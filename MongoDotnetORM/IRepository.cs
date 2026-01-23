66b43cd24b01ae4fc92035780b4c99f5f7ab166a
Sonam Jamtsho <sjjamtsho@gmail.com>
2026-01-22 22:17:43 -0500

    created baseentity class, repository interface and datasource

---
+++ MongoDotnetORM/IRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Linq.Expressions;
+
+namespace MongoDotnetORM
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> InsertAsync(T entity);
+        Task<List<T>> InsertManyAsync(IEnumerable<T> entities);
+        Task<T> FindByIdAsync(string id);
+        Task<List<T>> FindAllAsync();
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> filter);
+        Task<T> FindOneAsync(Expression<Func<T, bool>> filter);
+
+        Task<T> UpdateAsync(string id, T entity);
+        Task<bool> DeleteAsync(string id);
+
+        Task<long> DeleteManyAsync(Expression<Func<T, bool>> filter);
+
+        Task<long> CountAsync(Expression<Func<T, bool>> filter);
+        IQueryable<T> AsQueryable();
+    }
+}
---
+++ MongoDotnetORM/Repository.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDotnetORM
+{
+    public class Repository<T>: IRepository<T> where T : class
+    {
+        protected readonly IMongoCollection<T> _collection;
+
+        public Repository(IMongoDatabase db)
+        {
+            _collection = db.GetCollection<T>(typeof(T).Name + "s");
+        }
+        public async Task InsertAsync(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public async Task<List<T>> FindAllAsync()
+        {
+            return await _collection.Find(_=> true).ToListAsync();
+        }
+
+        public async Task<T> FindByIdAsync(string id)
+        {
+            return await _collection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await _collection.ReplaceOneAsync(x => x.Id == entity.Id, entity);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _collection.DeleteOneAsync(x => x.Id == id);
+        }
+    }
+}
