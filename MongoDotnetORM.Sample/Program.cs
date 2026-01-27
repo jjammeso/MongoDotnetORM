3fc2ad4aad0dfd25f0010554ab1eb63b75794360
Sonam Jamtsho <sjjamtsho@gmail.com>
2026-01-26 19:35:20 -0500

    Finished up the DataSource and the repository

---
+++ MongoDotnetORM.Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDotnetORM.Sample.Models;
+using src;
+
+async Task RunTestAsync()
+{
+    string connectionString = "mongodb://localhost:27017";
+    string databaseName = "YourDatabaseName";
+
+    var dataSrc = new DataSource(connectionString, databaseName);
+    var userRepo = dataSrc.GetRepository<User>();
+
+    var sonam = new User { Name = "Sonam", Email = "sjjamtsho@gmail.com", Password = "password" };
+
+    Console.WriteLine("The user below is about to be inserted:");
+    Console.WriteLine($"Name: {sonam.Name}, Email: {sonam.Email}, Password: {sonam.Password}");
+
+ 
+
+}
+
+
+await RunTestAsync();
+
+
+
--- src/DataSource.cs
+++ src/DataSource.cs
@@ -1,24 +1,31 @@
 ﻿using MongoDB.Driver;
 
-namespace MongoDotnetORM
+namespace src
 {
     public class DataSource
     {
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+
         public IMongoDatabase database;
 
-        string connectionString = "mongodb://localhost:27017";
-        string databaseName = "YourDatabaseName";
-
-        public DataSource()
+        public DataSource(string connectionString, string databaseName)
         {
             var client = new MongoClient(connectionString);
             database = client.GetDatabase(databaseName);
         }
 
-        public Repository<T> GetRepository<T>() where T : BaseEntity
+        public IRepository<T> GetRepository<T>() where T : class
         {
+            var type = typeof(T);
+
+            if(_repositories.ContainsKey(type))
+            {
+                return (IRepository<T>)_repositories[type];
+            }
+
             var collection = database.GetCollection<T>(typeof(T).ToString() + 's');
             var repo = new Repository<T>(collection);
+            _repositories[type] = repo;
             return repo;
         }
     }
